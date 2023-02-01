931ffc3ec111632eebc55478b5bb0d4d5698622f
Kim SungEun <kse8425z@gmail.com>
2023-02-01 22:06:09 +0900

    장고 admin을 통한 데이터 관리

---
+++ instagram/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
+    list_display_links = ['id', 'message']
